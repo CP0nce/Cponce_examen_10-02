568255eb64cd414fd8f68f4371f568be700f2bc2
CP0nce <cponce@cifpfbmoll.eu>
2022-02-10 18:08:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Examen 
+
+dejo aqui la configuración del .env
+
+
+DB_CONNECTION=pgsql
+
+DB_HOST=g1.ifc33b.cifpfbmoll.eu
+
+DB_PORT=5432
+
+DB_DATABASE=cponce_practica
+
+DB_USERNAME=cponce
+
+DB_PASSWORD=abc123.
---
+++ database/migrations/2022_02_10_165717_create_formulario.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormulario extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('formulario', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('extracto');
+            $table->text('contenido');
+            $table->string('checkbox');
+            $table->string('acceso');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('formulario');
+    }
+}
